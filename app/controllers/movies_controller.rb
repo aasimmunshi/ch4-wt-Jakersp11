190cabad46a46f72e34c90c560ac4c9dbd09bf1c
Jacob Patterson <jpatte80@uncc.edu>
2018-10-08 18:49:59 +0000

    Done with section 4.8

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -10,17 +10,13 @@ class MoviesController < ApplicationController
     @movies = Movie.all
     end
     
-    private
-    def movie_params
-    params.require(:movie).permit(:title, :rating, :description, :release_date)
-    end
-    
     def new
     @movie = Movie.new
     # default: render 'new' template
     end 
     
     def create
+    #@movie = Movie.create!(params[:movie]) #old way
     @movie = Movie.create!(movie_params)  # new way
     flash[:notice] = "#{@movie.title} was successfully created."
     redirect_to movies_path
@@ -42,6 +38,11 @@ class MoviesController < ApplicationController
       @movie.destroy
       flash[:notice] = "Movie '#{@movie.title}' deleted."
       redirect_to movies_path
+    end
+    
+    private
+    def movie_params
+    params.require(:movie).permit(:title, :rating, :description, :release_date)
     end
     
 end
